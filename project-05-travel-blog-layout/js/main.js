af7060eaacb3b079a397db015593fbbf57f921f0
diana nica <themindhelp@gmail.com>
2025-11-10 10:21:15 +0000

    Added project 05 - Travel Blog Layout

---
+++ project-05-travel-blog-layout/js/main.js
@@ -0,0 +1,7 @@
+// Newsletter form handler
+document
+  .getElementById("subscribeForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("🎉 Thanks for subscribing to Wanderlust!");
+  });
